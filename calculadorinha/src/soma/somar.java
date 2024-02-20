74c9b29ea2ca186fb7465a1f11f6b564a9cd2b16
962900 <962900@PITLAB2178805.senac.educ>
2024-02-20 07:41:29 -0300

    novo exercicio

---
+++ calculadorinha/src/soma/somar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soma;
+
+/**
+ *
+ * @author 962900
+ */
+public class somar {
+    public static int conta(int a,int b){
+    int x;
+    x = a+b;
+    return x;
+    }
+}
---
+++ calculadorinha/src/subtrai/subtrair.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package subtrai;
+
+/**
+ *
+ * @author 962900
+ */
+public class subtrair {
+    public static int conta(int a,int b){
+    int x;
+    x = a-b;
+    return x;
+    }
+}
